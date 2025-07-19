e6faa134431f0596355138cf7cf9f72042c95004
wndudrla1011 <wndudrla1011@naver.com>
2025-07-19 16:52:18 +0900

    created Status Enum Class

---
+++ backend/src/main/java/com/rootable/backend/domain/entity/Status.java
@@ -0,0 +1,16 @@
+package com.rootable.backend.domain.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Status {
+
+    PENDING("대기 중"),
+    APPROVED("승인됨"),
+    REJECTED("거절됨");
+
+    private final String label;
+
+}
