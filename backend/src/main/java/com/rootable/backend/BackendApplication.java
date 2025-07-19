c6e298ca5c3fe40698ec6b3dfc7a717cb35adc35
wndudrla1011 <wndudrla1011@naver.com>
2025-07-19 16:30:54 +0900

    active jpa auditing

--- backend/src/main/java/com/rootable/backend/BackendApplication.java
+++ backend/src/main/java/com/rootable/backend/BackendApplication.java
@@ -2,7 +2,9 @@ package com.rootable.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // Active JPA Auditing
 @SpringBootApplication
 public class BackendApplication {
 
